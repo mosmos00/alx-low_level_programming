41b6cc2d2a5a27dd7f6aed8c6c85aee4b89c8651
mosmos00 <61896595+mosmos00@users.noreply.github.com>
2022-07-27 10:14:15 +0300

    Add files via upload
    ff

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - aprogram that prints its name
+ * @argc: argument count
+ * @argv: argument vector -array of command arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
